026fa1a2384200fb6da5059b13a30cf238078fb7
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-05-01 03:13:22 +0330

    feat: add mobile menu to header

--- components/header/header.js
+++ components/header/header.js
@@ -1,26 +1,51 @@
 import { addClass, removeClass, toggleClass } from "../../shared/utils.js"
 
+const cover = document.querySelector('.cover')
+const searchBoxBtn = document.querySelector('.header__search-btn')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+const closeMobileMenu = () => {
+    removeClass('mobile-menu--open', mobileMenu)
+    removeClass('cover--show', cover)
+}
+
 const handleSearchBoxShow = () => {
-    const searchBoxBtn = document.querySelector('.header__search-btn')
     const globalSearchDropdown = document.querySelector('.haeder__global-search-dropdown-wrapper')
-    const cover = document.querySelector('.cover')
 
 
     searchBoxBtn.addEventListener('click', () => {
+        searchBoxBtn.style.zIndex = "10"
         addClass('haeder__global-search-dropdown-wrapper--show',globalSearchDropdown)
         addClass('cover--show', cover)
     })
     cover.addEventListener('click', () => {
-        toggleClass('haeder__global-search-dropdown-wrapper--show',globalSearchDropdown)
-        toggleClass('cover--show', cover)
+        removeClass('haeder__global-search-dropdown-wrapper--show',globalSearchDropdown)
+        closeMobileMenu()
     })
 }
 
 const handleOpenMobileMenu = () => {
-    const toggleBtn = document.querySelector('.header__mobile-toggle-btn')
+    const mobileMenuItems = document.querySelectorAll('.mobile-menu__list-item')
+    const cover = document.querySelector('.cover')
+    const mobileToggleBtn = document.querySelector('.header__mobile-toggle-btn')
+    const menuCloseBtn = document.querySelector('.mobile-menu__close-btn')
 
-    toggleBtn.addEventListener('click', () => {
-        toggleClass('toggle-icon--open', toggleBtn)
+
+    mobileMenuItems.forEach(mobileMenu => {
+        mobileMenu.addEventListener('click', () => {
+            toggleClass('mobile-menu__dropdown-list--open', mobileMenu.lastChild.previousElementSibling)
+            toggleClass('mobile-menu__item-angle-left--open', mobileMenu.firstChild.nextElementSibling.lastElementChild)
+        })
+    })
+
+    mobileToggleBtn.addEventListener('click', () => {
+        searchBoxBtn.style.zIndex = "0"
+        toggleClass('mobile-menu--open', mobileMenu)
+        toggleClass('cover--show', cover)
+    })
+
+    menuCloseBtn.addEventListener('click', () => {
+        closeMobileMenu()
     })
 }
 
