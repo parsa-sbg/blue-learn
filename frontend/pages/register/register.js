43ddbda1d92929a019ff40835a47c5847cf3f0a8
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-05-07 00:16:20 +0330

    fix : empty register inputs after register

--- frontend/pages/register/register.js
+++ frontend/pages/register/register.js
@@ -1,8 +1,8 @@
-import { registerNewUser , saveIntoLocalStorage} from "../../services/auth.js";
+import { registerNewUser, saveIntoLocalStorage } from "../../services/auth.js";
 import { showTimerSwal } from "../../shared/utils.js";
 
 
- const submitBtn = document.querySelector('.submit-btn')
+const submitBtn = document.querySelector('.submit-btn')
 
 window.addEventListener('load', () => {
 
@@ -11,7 +11,7 @@ window.addEventListener('load', () => {
 
         const nameInputValue = document.querySelector('#name-input').value
         const userNameInputValue = document.querySelector('#user-name-input').value
-        const emailInputValue = document.querySelector('#email-input').value       
+        const emailInputValue = document.querySelector('#email-input').value
         const phoneInputValue = document.querySelector('#phone-input').value
         const passwordInputValue = document.querySelector('#password-input').value
 
@@ -26,9 +26,16 @@ window.addEventListener('load', () => {
 
         console.log(response)
 
-        if(response.res.ok){
+        if (response.res.ok) {
             const userToken = response.data.accessToken
             saveIntoLocalStorage('userToken', userToken)
+
+            nameInputValue = ''
+            userNameInputValue = ''
+            emailInputValue = ''
+            phoneInputValue = ''
+            passwordInputValue = ''
+
             showTimerSwal(
                 'success',
                 'شما با موفقیت ثبت نام شدید',
@@ -38,27 +45,27 @@ window.addEventListener('load', () => {
                 }
             )
 
-        }else{
-            switch(response.data.message[0].name){
-                case 'phone': showTimerSwal('error', 'شماره تماس الزامی است', 'فهمیدم', ()=>{})
-                break;
-                
-                case 'email': showTimerSwal('error', 'ایمیل الزامی است', 'فهمیدم', ()=>{})
-                break;
+        } else {
+            switch (response.data.message[0].name) {
+                case 'phone': showTimerSwal('error', 'شماره تماس الزامی است', 'فهمیدم', () => { })
+                    break;
 
-                case 'username': showTimerSwal('error', 'نام کاربری الزامی است', 'فهمیدم', ()=>{})
-                break;
+                case 'email': showTimerSwal('error', 'ایمیل الزامی است', 'فهمیدم', () => { })
+                    break;
 
-                case 'name': showTimerSwal('error', 'نام و نام خانوادگی الزامی است', 'فهمیدم', ()=>{})
-                break;
+                case 'username': showTimerSwal('error', 'نام کاربری الزامی است', 'فهمیدم', () => { })
+                    break;
 
-                case 'password': showTimerSwal('error', 'رمز عبور الزامی است', 'فهمیدم', ()=>{})
-                break;
+                case 'name': showTimerSwal('error', 'نام و نام خانوادگی الزامی است', 'فهمیدم', () => { })
+                    break;
 
-                default : showTimerSwal('error', 'نام کاربری یا رمز عبور تکراری است', 'فهمیدم', ()=>{})
+                case 'password': showTimerSwal('error', 'رمز عبور الزامی است', 'فهمیدم', () => { })
+                    break;
+
+                default: showTimerSwal('error', 'نام کاربری یا رمز عبور تکراری است', 'فهمیدم', () => { })
             }
         }
 
     })
-    
+
 })
