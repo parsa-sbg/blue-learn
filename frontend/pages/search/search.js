a2ee79740e49bd2388b89ab1edef3606d5614d50
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-05-09 21:54:39 +0330

    coding when no result are found in global search

--- frontend/pages/search/search.js
+++ frontend/pages/search/search.js
@@ -6,6 +6,8 @@ window.addEventListener('load', async () => {
     const coursesWrapper = document.querySelector('.courses__courses-wrapper .row')
     const coursesCountElem = document.querySelector('.courses__count')
     const coursesTitle = document.querySelector('.courses__title')
+    const coursesSortBox = document.querySelector('.courses__sort-box')
+
 
     const searchValue = getUrlParam('search-value')
 
@@ -15,7 +17,13 @@ window.addEventListener('load', async () => {
         return course.name.toUpperCase().includes(searchValue.toUpperCase())
     })
 
-    renderCourseBoxInWrapper(filteredcourses, coursesWrapper, 'normal')
+    if (filteredcourses.length) {
+        renderCourseBoxInWrapper(filteredcourses, coursesWrapper, 'normal')
+    }else{
+        coursesSortBox.style.display = 'none'
+        coursesWrapper.innerHTML = '<div style="height: 30rem;" class="alert alert-danger text-center fs-1 d-flex align-items-center justify-content-center">نتیجه ای پیدا نشد</div>'
+    }
+
     coursesCountElem.innerHTML = `${filteredcourses.length} عنوان آموزشی`
 
     coursesTitle.innerHTML = `نتایج جستجوی ${searchValue} :`
