ce0fb8562b2746ece5e84a15e1b33024b194fa79
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-05-23 00:16:45 +0330

    delete course sessions after delete coure

--- panel/pages-panel/courses-panel/courses-panel.js
+++ panel/pages-panel/courses-panel/courses-panel.js
@@ -1,6 +1,6 @@
-import { showTimerSwal , showQuestionSwal } from "../../../shared/utils.js";
+import { showTimerSwal, showQuestionSwal, getUserToken } from "../../../shared/utils.js";
 import { getAllCatrgories } from "../../../services/categories.js";
-import { createNewCourse, getAllCourses , deleteCourse} from "../../../services/courses.js";
+import { createNewCourse, getAllCourses, deleteCourse, getCourseData, deleteSession } from "../../../services/courses.js";
 import { createLoader } from "../../../shared/loader.js";
 const loader = createLoader('در حال آپلود ...')
 
@@ -8,10 +8,9 @@ const createCourseBtn = document.querySelector('.form__btn')
 const categoryWrapper = document.querySelector('#category')
 const newCourseFileInput = document.querySelector('#cover')
 const allCatrgories = await getAllCatrgories()
-console.log(allCatrgories);
-let cover = null 
+let cover = null
 
-const  coursesTableBody = document.querySelector('.courses-table-body')
+const coursesTableBody = document.querySelector('.courses-table-body')
 
 
 const getAndShowAllCourses = async () => {
@@ -20,7 +19,7 @@ const getAndShowAllCourses = async () => {
 
 
     coursesTableBody.innerHTML = ''
-    coursesTableBody.insertAdjacentHTML('beforeend',`
+    coursesTableBody.insertAdjacentHTML('beforeend', `
                     <tr>
                         <th></th>
                         <th class="courses-table__header">نام دوره</th>
@@ -29,25 +28,31 @@ const getAndShowAllCourses = async () => {
                     </tr>
     `)
     allCourses.forEach((course, index) => {
-        console.log(course);
         const tr = document.createElement('tr')
         tr.insertAdjacentHTML('afterbegin', `
-            <td class="courses-table__item courses-table__item-number">${index +1 }</td>
+            <td class="courses-table__item courses-table__item-number">${index + 1}</td>
             <td class="courses-table__item">${course.name}</td>
             <td class="courses-table__item courses-table__item-price">${course.shortName}</td>
-            <td class="courses-table__delete-btn"><button onclick="showSwalAndDeleteCourse('${course._id}','${course.name}')">حذف</button></td>
+            <td class="courses-table__delete-btn"><button onclick="showSwalAndDeleteCourse('${course._id}','${course.name}','${course.shortName}')">حذف</button></td>
         `)
         coursesFragment.appendChild(tr)
     })
-    console.log('body');
     coursesTableBody.appendChild(coursesFragment)
 }
-    
-const showSwalAndDeleteCourse = (courseId , courseName) => {
-    console.log(courseId);
+
+const showSwalAndDeleteCourse = (courseId, courseName, courseShortName) => {
     showQuestionSwal('warning', `آیا از حذف دوره ${courseName} اطمینان دارید؟`, 'بله', 'حذف شد',
         async () => {
+            // delete course sessions 
+            const courseData = await getCourseData(courseShortName, getUserToken())
+            if (courseData.data.sessions) {
+                courseData.data.sessions.forEach(async session => {
+                    const res = await deleteSession(session._id)
+                })
+            }
+
             await deleteCourse(courseId)
+
             getAndShowAllCourses()
         }
     )
@@ -92,8 +97,8 @@ createCourseBtn.addEventListener('click', async event => {
         getAndShowAllCourses()
         showTimerSwal('success', 'دوره با موفقیت اضافه شد', 'فهمیدم', () => {
         })
-    }else {
-        showTimerSwal('error', response.data.message[0].message, 'فهمیدم', () => {})
+    } else {
+        showTimerSwal('error', response.data.message[0].message, 'فهمیدم', () => { })
     }
 })
 
